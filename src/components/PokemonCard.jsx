c0817a1b105bbb1a7cd78f40fb2fa5bc28b8d7eb
Vincent Ravanel <vravanel@gmail.com>
2023-08-26 15:19:44 +0200

    quete 03 ok

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,6 +1,25 @@
+const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
+
 function PokemmonCard() {
-    return <figure> <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" />   
-     <figcaption>bulbasaur</figcaption>
+    const pokemon = pokemonList[0];
+    let imageElement = null;
+    if (pokemon.imgSrc) {
+        imageElement = <img src={pokemon.imgSrc} alt={pokemon.name} />;
+     } else {
+        imageElement = <p> ??? </p>;
+     }
+    return <figure> 
+        {imageElement}  
+     <figcaption>{pokemon.name}</figcaption>
     </figure>
 }
 
